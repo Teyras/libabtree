ef643a5e9c983708cf8b6fcb6147c38c1190cc4a
Teyras <teyras@gmail.com>
2014-02-19 00:12:58 +0100

    bigger test

--- main.cpp
+++ main.cpp
@@ -3,16 +3,20 @@
 
 int main(int argc, char ** argv) {
 	abtree/*<int, std::string>*/ tree(2, 3);
-	abtree::iterator it;
+	abtree::iterator it = tree.begin();
 	
-	tree.insert(std::make_pair(5, std::string("foo")));
-	tree.insert(std::make_pair(6, std::string("foobar")));
-	tree.insert(std::make_pair(10, std::string("foovaz")));
+	int keys[] = {
+		9, 54, 68, 27, 18, 98, 74, 44, 1, 73, 
+		67, 75, 93, 92, 76, 69, 78, 4, 32, 52, 
+		81, 97, 71, 99, 87, 43, 53, 57, 30, 85, 
+		45, 95, 82, 63, 26, 65, 20, 28, 31, 94
+	};
+	
+	for (size_t i = 0; i < sizeof(keys) / sizeof(int); i++) {
+		tree.insert(std::make_pair(keys[i], std::string("foo")));
+	}
 	
 	std::cout << tree.size() << std::endl;
-	std::cout << tree.find(5)->second << std::endl;
-	std::cout << tree.find(6)->second << std::endl;
-	std::cout << tree.find(10)->second << std::endl;
 	
 	it = tree.begin();
 	for (size_t i = 0; i < 3; i++) {
