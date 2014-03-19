e9b1d80a24f782a4823a2faa72fe38ada2e27983
Jan Buchar <bucharj3@u-pl11.ms.mff.cuni.cz>
2014-03-19 13:10:01 +0100

    only allow the abtree container to construct a vertex

--- vertex.hpp
+++ vertex.hpp
@@ -3,6 +3,9 @@
 
 #include <iostream>
 
+template <typename TKey, typename TVal>
+class abtree;
+
 /**
  * A vertex of an (a, b)-tree
  */
@@ -13,24 +16,6 @@ struct abtree_vertex
 	size_t item_count;
 	std::pair<const TKey, TVal> ** items;
 	abtree_vertex ** children;
-	
-	/**
-	 * The default constructor. Allocates memory for given amount of items and children.
-	 * One more place in memory is allocated to simplify splitting.
-	 * @param max_children specifies the maximum amount of children
-	 */
-	abtree_vertex (size_t max_children): parent(nullptr), item_count(0)
-	{
-		items = new std::pair<const TKey, TVal> * [max_children];
-		children = new abtree_vertex<TKey, TVal> * [max_children + 1];
-		
-		for (size_t i = 0; i < max_children; i++) {
-			items[i] = nullptr;
-		}
-		for (size_t i = 0; i <= max_children; i++) {
-			children[i] = nullptr;
-		}
-	}
 	
 	~abtree_vertex ()
 	{
@@ -61,6 +46,28 @@ struct abtree_vertex
 		}
 		
 		return first;
+	}
+	
+private:
+	friend class abtree<TKey, TVal>;
+	
+	/**
+	 * The default constructor. Allocates memory for given amount of items and children.
+	 * One more place in memory is allocated to simplify splitting.
+	 * Only the abtree container is able to conctruct a vertex.
+	 * @param max_children specifies the maximum amount of children
+	 */
+	abtree_vertex (size_t max_children): parent(nullptr), item_count(0)
+	{
+		items = new std::pair<const TKey, TVal> * [max_children];
+		children = new abtree_vertex<TKey, TVal> * [max_children + 1];
+		
+		for (size_t i = 0; i < max_children; i++) {
+			items[i] = nullptr;
+		}
+		for (size_t i = 0; i <= max_children; i++) {
+			children[i] = nullptr;
+		}
 	}
 };
 
